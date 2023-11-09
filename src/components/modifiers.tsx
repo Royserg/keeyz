7ce38ba453cda6b88dd4607db60762cd6801d53e
Royserg <staniojakub@gmail.com>
2023-11-10 00:27:45 +0300

    style(CapsLock): make CapsLock darker

--- src/components/modifiers.tsx
+++ src/components/modifiers.tsx
@@ -1,19 +1,19 @@
-import { Show, onMount } from "solid-js";
+import { Show, onMount } from 'solid-js';
 
 // --- Move mouse on top of the application window ---
 // `mouseenter` will gets triggered to get status of CapsLock
-import { useStore } from "@nanostores/solid";
-import { LogicalPosition, appWindow } from "@tauri-apps/api/window";
-import { isCapsLockOn, setCapsLockStatus } from "../stores/modifiers";
+import { useStore } from '@nanostores/solid';
+import { LogicalPosition, appWindow } from '@tauri-apps/api/window';
+import { isCapsLockOn, setCapsLockStatus } from '../stores/modifiers';
 await appWindow.setCursorPosition(new LogicalPosition(50, 50));
 
 export const Modifiers = () => {
-  const body = document.querySelector("body")!;
+  const body = document.querySelector('body')!;
 
   const isCapsOn = useStore(isCapsLockOn);
 
   const mouseEventHandler = (e: MouseEvent) => {
-    const isCapsLockOn = e.getModifierState("CapsLock");
+    const isCapsLockOn = e.getModifierState('CapsLock');
 
     if (isCapsLockOn) {
       setCapsLockStatus(true);
@@ -21,19 +21,19 @@ export const Modifiers = () => {
       setCapsLockStatus(false);
     }
 
-    body.removeEventListener("mouseenter", mouseEventHandler);
+    body.removeEventListener('mouseenter', mouseEventHandler);
   };
 
   onMount(async () => {
     // Mouse event - Workaround to find Modifier key state (CapsLock)
-    // Should fire one time
-    body.addEventListener("mouseenter", mouseEventHandler);
+    // Should fire once
+    body.addEventListener('mouseenter', mouseEventHandler);
   });
 
   return (
     <div class="flex gap-2">
       <Show when={isCapsOn()}>
-        <div class="border-2 border-solid border- rounded-md p-2 bg-red-400 text-white font-bold">
+        <div class="border-r-4 border-r-red-800 border-b-4 border-b-red-950 border-solid rounded-md p-2 bg-red-600 text-gray-100 font-bold">
           CapsLock
         </div>
       </Show>
