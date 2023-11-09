8de9e2cd289e5bc54a698b2e2a293cac51cdf48e
Royserg <staniojakub@gmail.com>
2023-11-10 00:05:58 +0300

    refactor: move ClickedKey to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { UnlistenFn, listen } from '@tauri-apps/api/event';
-import { Component, For, createSignal, onCleanup, onMount } from 'solid-js';
+import { For, createSignal, onCleanup, onMount } from 'solid-js';
+import { ClickedKey } from './components/clicked-key';
 import { Modifiers } from './components/modifiers';
 import { KEY_EVENT, KeyEvent } from './interfaces/key.interface';
 import { handleClickedKey } from './services/keys';
+
+const MAX_KEYS = 4;
 
 function App() {
   const [keys, setKeys] = createSignal<string[]>([]);
@@ -10,7 +13,6 @@ function App() {
   let unlisten: UnlistenFn;
   onMount(async () => {
     unlisten = await listen(KEY_EVENT, (event: KeyEvent) => {
-      console.log('EVENT', event);
       const { key, event_type } = event.payload;
       const clickedKey = handleClickedKey({
         keyName: key,
@@ -18,7 +20,7 @@ function App() {
       });
 
       const updatedKeys = keys();
-      if (updatedKeys.length > 5) {
+      if (updatedKeys.length > MAX_KEYS) {
         updatedKeys.pop();
       }
 
@@ -34,8 +36,8 @@ function App() {
 
   return (
     <div class="container bg-black min-h-full h-24 p-1 rounded-sm">
-      <div class="flex flex-row-reverse gap-x-0.5">
-        <For each={keys()}>{(item) => <ClickedKey data={item}></ClickedKey>}</For>
+      <div class="flex flex-row-reverse gap-x-1">
+        <For each={keys()}>{(item) => <ClickedKey>{item}</ClickedKey>}</For>
       </div>
 
       <div class="py-1"></div>
@@ -46,15 +48,3 @@ function App() {
 }
 
 export default App;
-
-// -- ClickedKey
-interface ClickedKeyProps {
-  data: string;
-}
-const ClickedKey: Component<ClickedKeyProps> = (props) => {
-  return (
-    <div class="p-2 border-2 border-solid border-slate-300 rounded-md min-w-10 flex justify-center items-center bg-slate-200">
-      {props.data}
-    </div>
-  );
-};
